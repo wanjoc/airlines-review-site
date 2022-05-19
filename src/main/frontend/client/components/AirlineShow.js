453d3fcad28ab16af4e22cf9c40ec6b2f29f7767
evelm16 <eveladenmauro@gmail.com>
2022-05-19 15:18:11 -0400

    Created review form component and rendered it in airline showpage

--- src/main/frontend/client/components/AirlineShow.js
+++ src/main/frontend/client/components/AirlineShow.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react"
 import { useLocation, Link } from "react-router-dom"
 
 import ReviewList from "./ReviewList"
+import ReviewForm from "./ReviewForm"
 
 const AirlineShow = props => {
   let location = useLocation()
   const [airline, setAirline] = useState({reviews: []})
+  const [showReviewForm, setShowReviewForm] = useState(false)
+  const [successfulSubmission, setSuccessfulSubmission] = useState(false)
   const airlineId = props.match.params.id
 
   const fetchAirline = async () => {
@@ -27,9 +30,30 @@ const AirlineShow = props => {
     fetchAirline()
   }, [location.pathname])
 
+  let reviewForm = ""
+
+  const handleClick = (e) => {
+    if(!showReviewForm) {
+      setShowReviewForm(true)
+    } else {
+      setShowReviewForm(false)
+    }
+  } 
+
+  const success = output => {
+    setSuccessfulSubmission(output)
+    setShowReviewForm(false)
+  }
+
+  if(showReviewForm) {
+    reviewForm = <ReviewForm airlineId={airline.id} success={success} />
+  }
+
   return (
     <div>
       <h1 className="airline-title">{airline.name}</h1>
+      <button onClick={handleClick}>Add A Review</button>
+      {reviewForm}
       <p className="airline-description">{airline.description}</p>
       <img
         className="airline-logoUrl"
