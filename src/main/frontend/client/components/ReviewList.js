46e133cc9faa57efe90bcb35800f8605aaa8313e
evelm16 <eveladenmauro@gmail.com>
2022-05-23 13:48:49 -0400

    made changes to review index style

--- src/main/frontend/client/components/ReviewList.js
+++ src/main/frontend/client/components/ReviewList.js
@@ -10,18 +10,7 @@ const ReviewList = props => {
   return (
     <div>
       <h2>Reviews</h2>
-      <div className="input-field">
-        <div className="search">
-          <button type="submit" className="input-field">
-            <i className="fas fa-search"></i>
-            <input
-              id="search"
-              placeholder="Search for reviews ..."
-              type="text"
-            />
-          </button>
-        </div>
-      </div>
+      <div className="input-field"></div>
       <ul>{reviewTiles}</ul>
     </div>
   )
--- src/main/frontend/client/components/ReviewTile.js
+++ src/main/frontend/client/components/ReviewTile.js
@@ -5,30 +5,30 @@ import "./reviewIndex.scss"
 const ReviewTile = ({ review: { reviewerName, numberOfStars, comment } }) => {
   return (
     <div className="review-container">
-        <div className="cards">
-          <div className="card">
-            <div className="card-top">
-              <div className="name">
-                <div className="img one"></div>
-                <p>{reviewerName}</p>
-              </div>
-              <div className="rate">{numberOfStars}</div>
-            </div>
-          </div>
-          <div className="card-content">
-            <p>{comment}</p>
-          </div>
-          <div className="card-action">
-            <span>Date</span>
-            <button className="btn">
-              <i className="social-links"></i>
-              <a href="https://www.facebook.com/" className="fa fa-facebook"></a>
-              <a href="https://www.twitter.com/" className="fa fa-twitter"></a>
-              <a href="https://www.linkedin.com/" className="fa fa-linkedin"></a>
-              <a href="https://www.instagram.com/" className="fa fa-instagram"></a>
-            </button>
+      <div className="cards">
+        <div className="card">
+          <div className="card-top">
+            <p className="name">{reviewerName}</p>
+            <p className="rate">{numberOfStars}</p>
           </div>
         </div>
+        <div className="card-content">
+          <p>{comment}</p>
+        </div>
+        <div className="card-action">
+          <span></span>
+          <button className="btn">
+            <i className="social-links"></i>
+            <a href="https://www.facebook.com/" className="fa fa-facebook"></a>
+            <a href="https://www.twitter.com/" className="fa fa-twitter"></a>
+            <a href="https://www.linkedin.com/" className="fa fa-linkedin"></a>
+            <a
+              href="https://www.instagram.com/"
+              className="fa fa-instagram"
+            ></a>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
