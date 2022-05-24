a626560e87ce4f94560b0fa5b1269c763b9ba5fd
evelm16 <eveladenmauro@gmail.com>
2022-05-24 17:14:39 -0400

    delete

--- src/main/java/com/launchacademy/reviews/exceptionHandling/ReviewNotDeletedException.java
+++
@@ -1,8 +0,0 @@
-package com.launchacademy.reviews.exceptionHandling;
-
-public class ReviewNotDeletedException extends RuntimeException {
-
-  public ReviewNotDeletedException() {
-    super("Review cannot be deleted");
-  }
-}
